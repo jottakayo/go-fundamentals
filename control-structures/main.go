9977794daf01006da22c1e6e08814c231670ad42
Jotta Kayo M Lima <jottakayoml@gmail.com>
2026-08-01 15:35:28 -0300

    add conditional choese

--- control-structures/main.go
+++ control-structures/main.go
@@ -9,19 +9,32 @@ func main() {
   fmt.Printf("2. Deposit Money\n")
   fmt.Printf("3. Withdraw Money\n")
   fmt.Printf("4. Exit\n")
-
-  var choice int
-
-  fmt.Scanln(&choice)
-
-  checkBalance := choice ==1
-
-  if checkBalance {
-    fmt.Printf("Your balance is $1000\n")
-  }
-
   
+  var yourBalance float32 = 1000
+  var choice int
+  fmt.Printf("which one do you choose?\n")
+  fmt.Scanln(&choice)
+  // fmt.Printf("You chose option %d\n", choice)
+  // checkBalance := choice ==1
 
-  fmt.Printf("You chose option %d\n", choice)
+  if choice == 1 {
+    fmt.Printf("Your balance is %f\n", yourBalance)
+  } else if choice == 2 {
+    fmt.Printf("Your deposit: ")
+    var depositMount float32
+    fmt.Scan(&depositMount)
+    yourBalance += depositMount
+    fmt.Printf("Balancer update is: %.2f\n", yourBalance)
+  } else if choice == 3 {
+    fmt.Printf("How much money do you takeout?\n")
+    var withdrawMoney float32
+    fmt.Scan(&withdrawMoney)
+    yourBalance -= withdrawMoney
+    fmt.Printf("Balancer update is: %.2f\n", yourBalance)
+  } else if choice == 4 {
+    fmt.Printf("Ok, thank for coming here!\n")
+  } else{
+    fmt.Printf("This values is different of options that you may choese!\n")
+  }
 }
 
