81472919899da5bd5000a31b230fcb68b8e195e5
Jotta Kayo M Lima <jottakayoml@gmail.com>
2026-08-01 18:15:01 -0300

    add new flux for return in the menu

--- control-structures/main.go
+++ control-structures/main.go
@@ -5,8 +5,8 @@ import "fmt"
 func main() {  
   var yourBalance float32 = 1000
   var choice int
-  for i := 0; i < 3; i++ {
-    fmt.Printf("Welcome to Golang Bank!\n")
+  fmt.Printf("Welcome to Golang Bank!\n")
+  for {
     fmt.Printf("What do you want to do?\n")
     fmt.Printf("1. Check Balance\n")
     fmt.Printf("2. Deposit Money\n")
@@ -16,38 +16,38 @@ func main() {
     fmt.Scanln(&choice)
 
     if choice == 1 {
-      fmt.Printf("Your balance is %f\n", yourBalance)
+      fmt.Printf("\nYour balance is %.2f\n\n", yourBalance)
     } else if choice == 2 {
       fmt.Printf("Your deposit: ")
       var depositMount float32
       fmt.Scan(&depositMount)
-      // Nested if Statements
       if depositMount <= 0 {
         fmt.Printf("Invalid mount. Most be greather than 0.\n")
         return
       }
       yourBalance += depositMount
-      fmt.Printf("Balancer update is: %.2f\n", yourBalance)
+      fmt.Printf("\nBalancer update is: %.2f\n\n", yourBalance)
     } else if choice == 3 {
       fmt.Printf("How much money do you takeout?\n")
       var withdrawMoney float32
       fmt.Scan(&withdrawMoney)
-      // Nested if Statements
       if withdrawMoney <= 0 {
-        fmt.Printf("Invalid mount. Most be greather than 0.\n")
-        return
+        fmt.Printf("Invalid mount. Most be greather than 0. try again.\n")
+        continue
       }
       if withdrawMoney > yourBalance {
-        fmt.Printf("Invalid mount. Most be less than value in your account.\n")
-        return
+        fmt.Printf("Invalid mount. Most be less than value in your account. Check your balance.\n")
+        continue
       }
       yourBalance -= withdrawMoney
-      fmt.Printf("Balancer update is: %.2f\n", yourBalance)
+      fmt.Printf("\nBalancer update is: %.2f\n\n", yourBalance)
     } else if choice == 4 {
-      fmt.Printf("Ok, thank for coming here!\n")
+      fmt.Printf("Good bye!")
+      break
     } else{
-      fmt.Printf("This values is different of options that you may choese!\n")
+      fmt.Printf("\nThis values is different of options that you may choese!\n\n")
     }
   }
+  fmt.Printf("Thank for coming here!\n")
 }
 
